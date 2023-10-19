4ad3ceee65c69186666bc97341e6b3f4a41ea4ce
shawn edgell <shawn_edgell@hotmail.com>
2023-10-19 16:26:50 -0400

    added form functionality to edu/exp sections

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,32 @@ function App() {
   const [address, setAddress] = useState('');
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-  const [school, setSchool] = useState('');
-  const [degree, setDegree] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [location, setLocation] = useState('');
-  const [companyName, setCompanyName] = useState('');
-  const [positionTitle, setPositionTitle] = useState('');
-  const [experienceStartDate, setExperienceStartDate] = useState('');
-  const [experienceEndDate, setExperienceEndDate] = useState('');
-  const [experienceLocation, setExperienceLocation] = useState('');
-  const [experienceDescription, setExperienceDescription] = useState('');
+  const [educations, setEducations] = useState([]); // Array to store Education data
+  const [experiences, setExperiences] = useState([]); // Array to store Experience data
+
+  // Function to add an education to the array
+  const addEducation = () => {
+    setEducations([...educations, {}]);
+  };
+
+  // Function to remove an education from the array
+  const removeEducation = (index) => {
+    const updatedEducations = [...educations];
+    updatedEducations.splice(index, 1);
+    setEducations(updatedEducations);
+  };
+
+  // Function to add an experience to the array
+  const addExperience = () => {
+    setExperiences([...experiences, {}]);
+  };
+
+  // Function to remove an experience from the array
+  const removeExperience = (index) => {
+    const updatedExperiences = [...experiences];
+    updatedExperiences.splice(index, 1);
+    setExperiences(updatedExperiences);
+  };
 
   return (
     <div className="cv-app">
@@ -54,82 +69,170 @@ function App() {
         {/* Education */}
         <div className="section">
           <h3>Education</h3>
-          <input
-            type="text"
-            placeholder="School"
-            value={school}
-            onChange={(e) => setSchool(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Degree"
-            value={degree}
-            onChange={(e) => setDegree(e.target.value)}
-          />
-          <div className="education-dates">
-            <input
-              type="text"
-              placeholder="Start Date"
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="End Date"
-              value={endDate}
-              onChange={(e) => setEndDate(e.target.value)}
-            />
-          </div>
-          <input
-            type="text"
-            placeholder="Location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
+          {educations.map((education, index) => (
+            <div key={index}>
+              <input
+                type="text"
+                placeholder="School"
+                value={education.school || ''}
+                onChange={(e) => {
+                  const updatedEducations = [...educations];
+                  updatedEducations[index] = {
+                    ...updatedEducations[index],
+                    school: e.target.value,
+                  };
+                  setEducations(updatedEducations);
+                }}
+              />
+              <input
+                type="text"
+                placeholder="Degree"
+                value={education.degree || ''}
+                onChange={(e) => {
+                  const updatedEducations = [...educations];
+                  updatedEducations[index] = {
+                    ...updatedEducations[index],
+                    degree: e.target.value,
+                  };
+                  setEducations(updatedEducations);
+                }}
+              />
+              <div className="education-dates">
+                <input
+                  type="text"
+                  placeholder="Start Date"
+                  value={education.startDate || ''}
+                  onChange={(e) => {
+                    const updatedEducations = [...educations];
+                    updatedEducations[index] = {
+                      ...updatedEducations[index],
+                      startDate: e.target.value,
+                    };
+                    setEducations(updatedEducations);
+                  }}
+                />
+                <input
+                  type="text"
+                  placeholder="End Date"
+                  value={education.endDate || ''}
+                  onChange={(e) => {
+                    const updatedEducations = [...educations];
+                    updatedEducations[index] = {
+                      ...updatedEducations[index],
+                      endDate: e.target.value,
+                    };
+                    setEducations(updatedEducations);
+                  }}
+                />
+              </div>
+              <input
+                type="text"
+                placeholder="Location"
+                value={education.location || ''}
+                onChange={(e) => {
+                  const updatedEducations = [...educations];
+                  updatedEducations[index] = {
+                    ...updatedEducations[index],
+                    location: e.target.value,
+                  };
+                  setEducations(updatedEducations);
+                }}
+              />
+              <button onClick={() => removeEducation(index)}>Remove</button>
+            </div>
+          ))}
+          <button onClick={() => addEducation()}>Add Education</button>
         </div>
 
         {/* Experience */}
         <div className="section">
           <h3>Experience</h3>
-          <input
-            type="text"
-            placeholder="Company Name"
-            value={companyName}
-            onChange={(e) => setCompanyName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Position Title"
-            value={positionTitle}
-            onChange={(e) => setPositionTitle(e.target.value)}
-          />
-          <div className="experience-dates">
-            <input
-              type="text"
-              placeholder="Start Date"
-              value={experienceStartDate}
-              onChange={(e) => setExperienceStartDate(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="End Date"
-              value={experienceEndDate}
-              onChange={(e) => setExperienceEndDate(e.target.value)}
-            />
-          </div>
-          <input
-            type="text"
-            placeholder="Location"
-            value={experienceLocation}
-            onChange={(e) => setExperienceLocation(e.target.value)}
-          />
-          <textarea
-            placeholder="Description (optional)"
-            value={experienceDescription}
-            onChange={(e) => setExperienceDescription(e.target.value)}
-          />
+          {experiences.map((experience, index) => (
+            <div key={index}>
+              <input
+                type="text"
+                placeholder="Company Name"
+                value={experience.companyName || ''}
+                onChange={(e) => {
+                  const updatedExperiences = [...experiences];
+                  updatedExperiences[index] = {
+                    ...updatedExperiences[index],
+                    companyName: e.target.value,
+                  };
+                  setExperiences(updatedExperiences);
+                }}
+              />
+              <input
+                type="text"
+                placeholder="Position Title"
+                value={experience.positionTitle || ''}
+                onChange={(e) => {
+                  const updatedExperiences = [...experiences];
+                  updatedExperiences[index] = {
+                    ...updatedExperiences[index],
+                    positionTitle: e.target.value,
+                  };
+                  setExperiences(updatedExperiences);
+                }}
+              />
+              <div className="experience-dates">
+                <input
+                  type="text"
+                  placeholder="Start Date"
+                  value={experience.startDate || ''}
+                  onChange={(e) => {
+                    const updatedExperiences = [...experiences];
+                    updatedExperiences[index] = {
+                      ...updatedExperiences[index],
+                      startDate: e.target.value,
+                    };
+                    setExperiences(updatedExperiences);
+                  }}
+                />
+                <input
+                  type="text"
+                  placeholder="End Date"
+                  value={experience.endDate || ''}
+                  onChange={(e) => {
+                    const updatedExperiences = [...experiences];
+                    updatedExperiences[index] = {
+                      ...updatedExperiences[index],
+                      endDate: e.target.value,
+                    };
+                    setExperiences(updatedExperiences);
+                  }}
+                />
+              </div>
+              <input
+                type="text"
+                placeholder="Location"
+                value={experience.location || ''}
+                onChange={(e) => {
+                  const updatedExperiences = [...experiences];
+                  updatedExperiences[index] = {
+                    ...updatedExperiences[index],
+                    location: e.target.value,
+                  };
+                  setExperiences(updatedExperiences);
+                }}
+              />
+              <textarea
+                placeholder="Description (optional)"
+                value={experience.description || ''}
+                onChange={(e) => {
+                  const updatedExperiences = [...experiences];
+                  updatedExperiences[index] = {
+                    ...updatedExperiences[index],
+                    description: e.target.value,
+                  };
+                  setExperiences(updatedExperiences);
+                }}
+              />
+              <button onClick={() => removeExperience(index)}>Remove</button>
+            </div>
+          ))}
+          <button onClick={() => addExperience()}>Add Experience</button>
         </div>
-
       </div>
 
       {/* Right side: Display user input */}
@@ -141,25 +244,32 @@ function App() {
         <p>Phone Number: {phoneNumber}</p>
 
         <h2>Education</h2>
-        <p>School: {school}</p>
-        <p>Degree: {degree}</p>
-        <p>Start Date: {startDate}</p>
-        <p>End Date: {endDate}</p>
-        <p>Location: {location}</p>
+        {educations.map((education, index) => (
+          <div key={index}>
+            <p>School: {education.school}</p>
+            <p>Degree: {education.degree}</p>
+            <p>Start Date: {education.startDate}</p>
+            <p>End Date: {education.endDate}</p>
+            <p>Location: {education.location}</p>
+          </div>
+        ))}
 
         <h2>Experience</h2>
-        <p>Company Name: {companyName}</p>
-        <p>Position Title: {positionTitle}</p>
-        <p>Start Date: {experienceStartDate}</p>
-        <p>End Date: {experienceEndDate}</p>
-        <p>Location: {experienceLocation}</p>
-        {experienceDescription && (
-          <div className="description">
-            <h3>Description</h3>
-            <p>{experienceDescription}</p>
+        {experiences.map((experience, index) => (
+          <div key={index}>
+            <p>Company Name: {experience.companyName}</p>
+            <p>Position Title: {experience.positionTitle}</p>
+            <p>Start Date: {experience.startDate}</p>
+            <p>End Date: {experience.endDate}</p>
+            <p>Location: {experience.location}</p>
+            {experience.description && (
+              <div className="description">
+                <h4>Description</h4>
+                <p>{experience.description}</p>
+              </div>
+            )}
           </div>
-        )}
-
+        ))}
       </div>
     </div>
   );
