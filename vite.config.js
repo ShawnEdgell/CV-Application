3b09d4d46171a858e2140d7dea0c63ceb10a7389
shawn edgell <shawn_edgell@hotmail.com>
2023-10-20 03:19:06 -0400

    deployed with firebase

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()
-  ],
-  base: '/CV-Application/',
+  plugins: [react()],
+  base: '/', // Set the base path to '/' if deploying to the root domain
   resolve: {
     alias: {
-      '@': '/src'
-    }
+      '@': 'src', // Remove the leading slash
+    },
   },
   build: {
-    target: 'esnext', 
-    outDir: 'dist', 
+    target: 'esnext',
+    outDir: 'public',
     minify: 'terser',
-    sourcemap: true
+    sourcemap: true,
   },
   server: {
-    port: 3000, 
-    open: true,   
-    strictPort: false, 
-    cors: true,   
-    https: false,  
-    proxy: { 
-    }
+    port: 3000,
+    open: true,
+    strictPort: false,
+    cors: true,
+    https: false,
+    proxy: {},
   },
-  
 });
